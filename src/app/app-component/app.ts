8381d42ae2851a6b57834bc24a67939b118cfc08
John McDonald <mcdona22@gmail.com>
2025-11-08 12:34:48 +0000

    checked navigation and made some standard page layout stuff available

--- src/app/app-component/app.ts
+++ src/app/app-component/app.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbar } from '@angular/material/toolbar';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+const menuOptions = [
+  { label: 'Home', path: '' },
+  { label: 'Gangs', path: '/gangs' },
+];
 
 @Component({
-  imports: [MatSidenavModule, MatToolbar],
+  imports: [
+    MatSidenavModule,
+    MatToolbar,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+  ],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
 })
-export class App {}
+export class App {
+  menuOptions = menuOptions;
+}
---
+++ src/app/features/gangs/gangs.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gangs',
+  imports: [],
+  templateUrl: './gangs.html',
+  styleUrl: './gangs.scss',
+})
+export class Gangs {
+
+}
