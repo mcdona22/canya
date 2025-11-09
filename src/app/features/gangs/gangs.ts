4966f3166804ff98a379d9a148e1b04591e5f020
John McDonald <mcdona22@gmail.com>
2025-11-09 12:14:08 +0000

    added auth and google sign in

--- src/app/features/gangs/gangs.ts
+++ src/app/features/gangs/gangs.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
+import { AuthService } from '../auth/application/auth-service';
+import { MatButton } from '@angular/material/button';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-gangs',
-  imports: [],
+  imports: [MatButton, JsonPipe],
   templateUrl: './gangs.html',
   styleUrl: './gangs.scss',
 })
 export class Gangs {
+  authService = inject(AuthService);
+  isAuthenticated = this.authService.authenticated;
+  currentUser = this.authService.currentUser;
 
+  constructor() {
+    effect(() => console.log(`Auth state has changed`, this.isAuthenticated()));
+    effect(() => console.log(`User changed`, this.currentUser()));
+  }
+
+  toggleAuthenticated() {
+    this.isAuthenticated()
+      ? this.authService
+          .signOut()
+          .then(() => console.log(`Signed out successfully`))
+      : this.authService
+          .signInWithGoogle()
+          .then(() => console.log(`Signed in with google`));
+  }
 }
