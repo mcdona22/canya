6e4b2f2143c864102e1dfc0caafa9e513222c7c1
John McDonald <mcdona22@gmail.com>
2025-11-09 01:33:34 +0000

    firebase providers in their own file and object

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -5,30 +5,13 @@ import {
 } from '@angular/core';
 import { routes } from '../routing/app-routes';
 import { provideRouter } from '@angular/router';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { LogLevel, setLogLevel } from '@angular/fire';
-import { environment } from '../../environments/environment';
-import { connectAuthEmulator, getAuth, provideAuth } from '@angular/fire/auth';
+import { firebaseProviders } from './firebase-providers';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideFirebaseApp(() => {
-      setLogLevel(LogLevel.VERBOSE);
-      console.log(`Initialising Firebase App`);
-      return initializeApp(environment.firebase);
-    }),
-
-    provideAuth(() => {
-      console.log(`Providing Authentication`);
-      const auth = getAuth();
-      if (environment.useEmulators) {
-        console.log(`Using Auth emulator`);
-        connectAuthEmulator(auth, 'http://localhost:9099');
-      }
-      return auth;
-    }),
+    ...firebaseProviders,
   ],
 };
