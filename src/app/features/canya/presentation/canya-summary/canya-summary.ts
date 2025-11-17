c864077bf53664fd968da729a1bc061a0a734f0a
John McDonald <mcdona22@gmail.com>
2025-11-17 16:55:19 +0000

    removed the tests

--- src/app/features/canya/presentation/canya-summary/canya-summary.ts
+++ src/app/features/canya/presentation/canya-summary/canya-summary.ts
@@ -2,13 +2,12 @@ import { Component, input } from '@angular/core';
 import { MatListItem } from '@angular/material/list';
 import { MatIcon } from '@angular/material/icon';
 import { CanyaEvent } from '../../data/CanyaEvent';
-import { DatePipe, JsonPipe } from '@angular/common';
 
 class Canya {}
 
 @Component({
   selector: 'app-canya-summary',
-  imports: [MatListItem, MatIcon, DatePipe, JsonPipe],
+  imports: [MatListItem, MatIcon],
   templateUrl: './canya-summary.html',
   styleUrl: './canya-summary.scss',
 })
